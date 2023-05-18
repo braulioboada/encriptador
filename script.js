4181227a81ef47cd27a6737b8c54c6ab2d6760e9
Braulio <brathlowiis@gmail.com>
2023-05-18 18:54:09 -0300

    Comenzando a crear el codigo de JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+console.table(matrizCodigo)
