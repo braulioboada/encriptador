69e67a963b9e8261da0ffbfb88e691b80dc534e1
Braulio <brathlowiis@gmail.com>
2023-05-18 19:20:23 -0300

    Creando las matrices de encriptacion y haciendo funcional los botones de encriptar y desencriptar

--- script.js
+++ script.js
@@ -7,5 +7,42 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"*/
 
-let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-console.table(matrizCodigo)
+function btmEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+
+function encriptar(stringEncritada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncritada = stringEncritada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncritada.includes(matrizCodigo[i][0])){
+            stringEncritada =stringEncritada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+        
+    }
+    return stringEncritada
+}
+
+function btmDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencritada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencritada = stringDesencritada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencritada.includes(matrizCodigo[i][1])){
+            stringDesencritada =stringDesencritada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+        
+    }
+    return stringDesencritada
+}
