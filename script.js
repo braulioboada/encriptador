4c407e9e68feaab2fe195be4c321897889f2c262
Braulio <brathlowiis@gmail.com>
2023-05-25 19:50:04 -0300

    terminando detalles

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
-/*La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"*/
-const keys = {a: "ai", e: "enter", i: "imes", o: "ober", u: "ufat",};
-
 function btmEncriptar(){
-    const input = document.getElementById("input").value;
+    const input = document.getElementById("input").value.toLowerCase();
     const mensaje = encriptar(input);
     if (input) {
 		document.getElementById("info").style.display = "none";
 		document.getElementById("resultadoMensaje").style.display = "block";
 		document.getElementById("btn-copiar").style.display = "block";
+		document.getElementById("input").value = "";
 	} else {
 		document.getElementById("info").style.display = "block";
 		document.getElementById("resultadoMensaje").style.display = "none";
 		document.getElementById("btn-copiar").style.display = "none";
+		document.getElementById("Munheco").src = "./imagenes/encriptado.jpg"
+		swal("Debe ingresar un texto")
 	}
 	document.getElementById("resultadoMensaje").value = mensaje;
 }
+function encriptar(stringEncritada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncritada = stringEncritada.toLowerCase()
 
-function encriptar(text) {
-	let result = "";
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncritada.includes(matrizCodigo[i][0])){
+            stringEncritada =stringEncritada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
 
-	for (let i = 0; i < text.length; i++) {
-		const char = text.charAt(i);
-
-		if (keys[char]) {
-			result += keys[char];
-		} else {
-			result += char;
-		}
-	}
-    return result;
+    }
+    return stringEncritada
 }
-
 function btmDesencriptar(){
     const input = document.getElementById("input").value;
     const mensaje = desencriptar(input);
@@ -42,27 +34,32 @@ function btmDesencriptar(){
 		document.getElementById("info").style.display = "none";
 		document.getElementById("resultadoMensaje").style.display = "block";
 		document.getElementById("btn-copiar").style.display = "block";
+		document.getElementById("input").value = "";
 	} else {
 		document.getElementById("info").style.display = "block";
 		document.getElementById("resultadoMensaje").style.display = "none";
 		document.getElementById("btn-copiar").style.display = "none";
+		document.getElementById("Munheco").src = "./imagenes/encriptado.jpg"
+		swal("Debe ingresar un texto")
 	}
 	document.getElementById("resultadoMensaje").value = mensaje;
 }
+function desencriptar(stringDesencritada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencritada = stringDesencritada.toLowerCase()
 
-function desencriptar(text) {
-	const keysArray = Object.entries(keys);
-	let result = text;
-	keysArray.forEach(([key, value]) => {
-		const regex = new RegExp(value, "g");
-		result = result.replace(regex, key);
-	});
-	return result;
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencritada.includes(matrizCodigo[i][1])){
+            stringDesencritada =stringDesencritada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+
+    }
+    return stringDesencritada
 }
-
 function btmCopiar(){
 	const mensaje = document.getElementById("resultadoMensaje");
     mensaje.select()
     mensaje.setSelectionRange(0, 99999);
     document.execCommand("copy")
+	swal("Texto copiado")
 }
