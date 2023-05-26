665190e509e4044d93a4a12bac0ed7dbff25224c
Braulio <brathlowiis@gmail.com>
2023-05-25 22:44:12 -0300

    colocandole la limitante de mayusculas y acento ej JS

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-function btmEncriptar(){
-    const input = document.getElementById("input").value.toLowerCase();
-    const mensaje = encriptar(input);
-    if (input) {
+function btmEncriptar() {
+	const input = document.getElementById("input").value
+    let condicion = /^[a-z\s]+$/;
+    if(input.match(condicion)){
+		const mensaje = encriptar(input);
 		document.getElementById("info").style.display = "none";
 		document.getElementById("resultadoMensaje").style.display = "block";
 		document.getElementById("btn-copiar").style.display = "block";
 		document.getElementById("input").value = "";
-	} else {
-		document.getElementById("info").style.display = "block";
-		document.getElementById("resultadoMensaje").style.display = "none";
-		document.getElementById("btn-copiar").style.display = "none";
-		document.getElementById("Munheco").src = "./imagenes/encriptado.jpg"
-		swal("Debe ingresar un texto")
+		document.getElementById("resultadoMensaje").value = mensaje;
 	}
-	document.getElementById("resultadoMensaje").value = mensaje;
+    else{
+        swal("Debe ingresar solo letras minúsculas y sin acentos")
+		document.getElementById("Munheco").src = "./imagenes/encriptado.jpg"
+    }
+}
+function btmDesencriptar() {
+	const input = document.getElementById("input").value
+    let condicion = /^[a-z\s]+$/;
+    if(input.match(condicion)){
+		const mensaje = desencriptar(input);
+		document.getElementById("info").style.display = "none";
+		document.getElementById("resultadoMensaje").style.display = "block";
+		document.getElementById("btn-copiar").style.display = "block";
+		document.getElementById("input").value = "";
+		document.getElementById("resultadoMensaje").value = mensaje;
+	}
+    else{
+        swal("Debe ingresar solo letras minúsculas y sin acentos")
+		document.getElementById("Munheco").src = "./imagenes/encriptado.jpg"
+    }
 }
 function encriptar(stringEncritada){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncritada = stringEncritada.toLowerCase()
+    stringEncritada = stringEncritada
 
     for (let i = 0; i < matrizCodigo.length; i++) {
         if(stringEncritada.includes(matrizCodigo[i][0])){
@@ -27,26 +42,9 @@ function encriptar(stringEncritada){
     }
     return stringEncritada
 }
-function btmDesencriptar(){
-    const input = document.getElementById("input").value;
-    const mensaje = desencriptar(input);
-    if (input) {
-		document.getElementById("info").style.display = "none";
-		document.getElementById("resultadoMensaje").style.display = "block";
-		document.getElementById("btn-copiar").style.display = "block";
-		document.getElementById("input").value = "";
-	} else {
-		document.getElementById("info").style.display = "block";
-		document.getElementById("resultadoMensaje").style.display = "none";
-		document.getElementById("btn-copiar").style.display = "none";
-		document.getElementById("Munheco").src = "./imagenes/encriptado.jpg"
-		swal("Debe ingresar un texto")
-	}
-	document.getElementById("resultadoMensaje").value = mensaje;
-}
 function desencriptar(stringDesencritada){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencritada = stringDesencritada.toLowerCase()
+    stringDesencritada = stringDesencritada
 
     for (let i = 0; i < matrizCodigo.length; i++) {
         if(stringDesencritada.includes(matrizCodigo[i][1])){
